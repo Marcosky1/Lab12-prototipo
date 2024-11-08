02fe374d702e9477eb398dcf3bf36686f10dd767
JoaquinCL <144145890+Marcosky1@users.noreply.github.com>
2024-11-08 09:29:57 -0500

    ParticleSystem

--- Assets/Particle/ParticleSystem.cs
+++ Assets/Particle/ParticleSystem.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParticleSystem : MonoBehaviour
+public class ParticleSystem
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<Particle> particles = new List<Particle>();
+    private ParticleFactory factory = new ParticleFactory();
+
+    public void AddParticle(Vector3 position, Vector3 velocity, float lifetime, string color, string texture)
     {
-        
+        var material = factory.GetMaterial(color, texture);
+        Particle particle = new Particle(position, velocity, lifetime, material);
+        particles.Add(particle);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        foreach (var particle in particles)
+        {
+            particle.Update();
+        }
+    }
+
+    public void Draw()
+    {
+        foreach (var particle in particles)
+        {
+            particle.Draw();
+        }
     }
 }
+
